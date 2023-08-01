eebcfa3001e4729e8e8a28a9ceef5d5b471099ba
anilmawji <mawjianil1@gmail.com>
2023-08-01 07:08:50 -0600

    Script executor cleanup

--- ITPortal.Lib/Utils/StringExtensions.cs
+++ ITPortal.Lib/Utils/StringExtensions.cs
@@ -2,8 +2,6 @@
 
 public static class StringExtensions
 {
-    private static readonly char[] defaultSeparators = { ',', ' ' };
-
     public static string EncodeBase64(this string plainText)
     {
         if (string.IsNullOrEmpty(plainText))
@@ -22,32 +20,5 @@ public static class StringExtensions
         }
         var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
         return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-    }
-
-    public static object? ConvertToArray(this string text, Type targetElementType)
-    {
-        return ConvertToArray(text, defaultSeparators, targetElementType);
-    }
-
-    public static object? ConvertToArray(this string text, char[] separators, Type targetElementType)
-    {
-        string[] valueArray = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-        // No need for further operations if the desired array type is string[]
-        if (targetElementType == typeof(string))
-        {
-            return valueArray;
-        }
-
-        try
-        {
-            object[] convertedArray = Array.ConvertAll(valueArray, elem => Convert.ChangeType(elem, targetElementType));
-
-            return convertedArray;
-        }
-        catch (Exception)
-        {
-            return null;
-        }
     }
 }
