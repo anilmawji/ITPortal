288f162768e78b40cafcd0f0ff6dd4bb6d62a074
anilmawji <mawjianil1@gmail.com>
2023-08-04 19:19:13 -0600

    Set default window size

--- ITPortal.Lib/Utils/DeviceWindowHandler.cs
+++ ITPortal.Lib/Utils/DeviceWindowHandler.cs
@@ -2,5 +2,8 @@
 
 public static class DeviceWindowHandler
 {
+    public const int WindowWidth = 1000;
+    public const int WindowHeight = 800;
+
     public static IntPtr WindowHandle { get; set; }
 }
--- ITPortal/App.xaml.cs
+++ ITPortal/App.xaml.cs
@@ -1,22 +1,34 @@
 ﻿using ITPortal.Lib.Utils;
 
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
 namespace ITPortal;
 
 public partial class App : Application
 {
-	public App()
+    public App()
 	{
 		InitializeComponent();
 
-#if WINDOWS
         Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
         {
+#if WINDOWS
             var nativeWindow = handler.PlatformView;
             nativeWindow.Activate();
-            DeviceWindowHandler.WindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-    });
-#endif
 
-        MainPage = new MainPage();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            DeviceWindowHandler.WindowHandle = windowHandle;
+
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(DeviceWindowHandler.WindowWidth, DeviceWindowHandler.WindowHeight));
+#endif
+        });
+
+            MainPage = new MainPage();
     }
 }
