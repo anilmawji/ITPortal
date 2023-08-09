ea9d8a2c31182982ceb1c1dbba4d3f918ebcf1b9
anilmawji <mawjianil1@gmail.com>
2023-08-09 00:50:11 -0600

    Moved add job method to interface

--- ITPortal.Lib/Services/Automation/JobTest/IScriptJobService.cs
+++ ITPortal.Lib/Services/Automation/JobTest/IScriptJobService.cs
@@ -1,4 +1,5 @@
 ﻿using ITPortal.Lib.Services.Automation.Script;
+using ITPortal.Lib.Utils;
 
 namespace ITPortal.Lib.Services.Automation.JobTest;
 
@@ -7,5 +8,13 @@ public interface IScriptJobService
     public int JobIdLength { get; set; }
     public Dictionary<string, ScriptJob> Jobs { get; set; }
 
-    public ScriptJob AddNewJob(AutomationScript script, int jobIdLength);
+    public ScriptJob AddNewJob(AutomationScript script, int jobIdLength)
+    {
+        string jobId = UniqueGuidGenerator.NewUniqueGuid(Jobs.Keys.ToList(), jobIdLength);
+        ScriptJob job = new(jobId, script);
+
+        Jobs.Add(jobId, job);
+
+        return job;
+    }
 }
--- ITPortal.Lib/Services/Automation/JobTest/PowerShellJobService.cs
+++ ITPortal.Lib/Services/Automation/JobTest/PowerShellJobService.cs
@@ -1,7 +1,4 @@
-﻿using ITPortal.Lib.Services.Automation.Script;
-using ITPortal.Lib.Utils;
-
-namespace ITPortal.Lib.Services.Automation.JobTest;
+﻿namespace ITPortal.Lib.Services.Automation.JobTest;
 
 public class PowerShellJobService : IScriptJobService
 {
@@ -11,15 +8,5 @@ public class PowerShellJobService : IScriptJobService
     public PowerShellJobService(int jobIdLength)
     {
         JobIdLength = jobIdLength;
-    }
-
-    public ScriptJob AddNewJob(AutomationScript script, int jobIdLength)
-    {
-        string jobId = UniqueGuidGenerator.NewUniqueGuid(Jobs.Keys.ToList(), jobIdLength);
-        ScriptJob job = new(jobId, script);
-
-        Jobs.Add(jobId, job);
-
-        return job;
     }
 }
