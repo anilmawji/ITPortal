3c8862dc204b72a1b40a7ff0aacdabaf6f2d7d03
anilmawji <mawjianil1@gmail.com>
2023-08-27 13:30:48 -0600

    Run job button and confirmation dialog

---
+++ ITPortal.Components/Models/DialogBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ITPortal.Components.Models;
+
+public class DialogBase : ComponentBase
+{
+    [CascadingParameter, EditorRequired]
+    MudDialogInstance? MudDialog { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string? ContentText { get; set; }
+
+    [Parameter]
+    public string SubmitButtonText { get; set; } = "Submit";
+
+    [Parameter]
+    public Color Color { get; set; }
+
+    internal void Submit() => MudDialog?.Close(DialogResult.Ok(true));
+    internal void Cancel() => MudDialog?.Cancel();
+}
